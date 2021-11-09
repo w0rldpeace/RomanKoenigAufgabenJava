e6b3568181a8432944d40d2c475d750a1f1cf36e
Roman Koenig <rk.home42@gmail.com>
2021-11-09 16:37:11 +0100

    Fertie Implementierung

---
+++ src/com/Tests/PointTest.java
@@ -0,0 +1,34 @@
+package com.Tests;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.point.Point;
+import org.junit.jupiter.api.Test;
+
+public class PointTest {
+
+    Point a = new Point(8.0,4.0, "Alpha");
+    Point b = new Point(2.0,2.0,"Bravo");
+    Point c;
+
+    @Test
+    public void testAddition(){
+        c = a.addCoordinates(a, b, "Charlie");
+        assertEquals(10.0, c.getX());
+        assertEquals(6.0, c.getY());
+    }
+    @Test
+    public void testSubtraction(){
+        c = a.subtractCoordinates(a,b,"Delta");
+        assertEquals(6.0, c.getX());
+        assertEquals(2.0, c.getY());
+    }    @Test
+    public void testMultiplication(){
+        c = a.multiplyCoordinates(a, b, "Echo");
+        assertEquals(16.0, c.getX());
+        assertEquals(8.0, c.getY());
+    }    @Test
+    public void testDivision(){
+        c = a.divideCoordinates(a, b, "Foxtrot");
+        assertEquals(4.0, c.getX());
+        assertEquals(2.0, c.getY());
+    }
+}
--- src/com/point/Point.java
+++ src/com/point/Point.java
@@ -1,4 +1,84 @@
 package com.point;
 
 public class Point {
+
+    private Double x;
+    private Double y;
+    private String name;
+
+    //Leerer Konstruktor
+    public Point(){
+
+    }
+    //Hauptkonstruktor
+    public Point(Double x, Double y, String name){
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //die toString Methode ist hier ueberschrieben und auf meine Felder angewendet per IDE
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    /**
+     *
+     * @param a Erster Punkt
+     * @param b Zweiter Punkt
+     * @param name Name des neuen Punkts
+     * @return Gibt einen neuen Punkt vom Typ "Point" zurueck
+     */
+    public Point addCoordinates(Point a, Point b, String name){
+        x = a.getX() + b.getX();
+        y = a.getY() + b.getY();
+        return new Point(x, y, name);
+    }
+
+    public Point subtractCoordinates(Point a, Point b, String name){
+        x = a.getX() - b.getX();
+        y = a.getY() - b.getY();
+        return new Point(x, y, name);
+    }
+
+    public Point multiplyCoordinates(Point a, Point b, String name){
+        x = a.getX() * b.getX();
+        y = a.getY() * b.getY();
+        return new Point(x, y, name);
+    }
+
+    public Point divideCoordinates(Point a, Point b, String name){
+        x = a.getX() / b.getX();
+        y = a.getY() / b.getY();
+        return new Point(x, y, name);
+    }
 }
