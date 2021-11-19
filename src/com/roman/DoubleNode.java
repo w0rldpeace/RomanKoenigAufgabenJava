0b3eadcd89fbf063f045b942319a41c04bc8772c
Roman Koenig <rk.home42@gmail.com>
2021-11-19 12:07:21 +0100

    Aufgabe 17 (Erweiterung) Initial Commit

--- src/com/roman/DoubleNode.java
+++ src/com/roman/DoubleNode.java
@@ -1,6 +1,41 @@
 package com.roman;
 
-public class DoubleNode {
+public class DoubleNode<T> {
     //
 
+    private T data;
+    private DoubleNode<T> prev;
+    private DoubleNode<T> next;
+
+    public DoubleNode(T data){
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public DoubleNode<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(DoubleNode<T> prev) {
+        this.prev = prev;
+    }
+
+    public DoubleNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(DoubleNode<T> next) {
+        this.next = next;
+    }
+
+    public void displayNodeData(){
+        System.out.print(data + " -> ");
+    }
 }
--- src/com/roman/DoublyLinkedList.java
+++ src/com/roman/DoublyLinkedList.java
@@ -1,5 +1,16 @@
 package com.roman;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<T> {
     //
+    private DoubleNode<T> head;
+
+
+    public DoublyLinkedList(DoubleNode head) {
+        this.head = head;
+    }
+
+    //Fuege einen neuen Knoten am Anfang der Liste
+    public void push(T data){
+
+    }
 }
